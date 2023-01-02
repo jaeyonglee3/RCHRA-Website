7b98c5ef9a5765daf44e37798b421a69adafce1c
Jaeyong Lee <lee.j1533@gmail.com>
2023-01-02 12:23:56 -0500

    code cleanup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,5 @@
 /**
-* Template Name: Reveal - v4.9.1
-* Template URL: https://bootstrapmade.com/reveal-bootstrap-corporate-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
+Main JavaScript file
 */
 
 // @ts-check
